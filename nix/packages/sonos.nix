006ac82da07c3b3a852fc37f89d631f2038b63ea
Martin Madsen <mj@factbird.com>
2024-03-12 23:46:30 +0100

    Remove trace expressions

--- nix/packages/sonos.nix
+++ nix/packages/sonos.nix
@@ -5,13 +5,10 @@
   stdenv,
   pkgs,
 }: {craneExtraArgs ? {}}: let
-  rustToolchain = builtins.trace pkgs.pkgsBuildHost.system (pkgs.pkgsBuildHost.rust-bin.fromRustupToolchainFile (src + /rust-toolchain.toml));
+  rustToolchain = pkgs.pkgsBuildHost.rust-bin.fromRustupToolchainFile (src + /rust-toolchain.toml);
   craneLib = (crane.mkLib pkgs.pkgsBuildHost).overrideToolchain rustToolchain;
 in
-  craneLib.buildPackage (builtins.trace ({
-        src = craneLib.cleanCargoSource (craneLib.path src);
-      }
-      // craneExtraArgs) {
-      src = craneLib.cleanCargoSource (craneLib.path src);
-    }
-    // craneExtraArgs)
+  craneLib.buildPackage {
+    src = craneLib.cleanCargoSource (craneLib.path src);
+  }
+  // craneExtraArgs
